3e174dd88b61853c2fd5fdc837706c240a63d3ea
Jorge Teixeira <jfct2013@gmail.com>
2018-12-09 16:12:20 +0000

    Day6
    Part2

--- day6/day6.js
+++ day6/day6.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    // Part 1 and 2 together
     let result          = day6();
 });
 
@@ -10,12 +11,13 @@ function day6() {
     input.onreadystatechange = function () {
         if(input.readyState === 4) {
             if(input.status === 200 || input.status == 0) {
-                let input2      = input.responseText.split('\n').map(r => numbers(r)),
-                    coord       = input.responseText.split('\n'),
+                let coord       = input.responseText.split('\n'),
+                    input2      = input.responseText.split('\n').map(r => numbers(r)),
                     edge        = [],
                     closest     = {},
                     max_x       = 0,
                     max_y       = 0,
+                    inRegion    = 0,
                     t0, t1;
 
                 for(element of coord) {
@@ -28,12 +30,17 @@ function day6() {
 
                 for (let index_x = 0; index_x < max_x+1; index_x++) {
                   for (let index_y = 0; index_y < max_y+1; index_y++) {
-                    let curPoint = [index_x, index_y];
-                    let distances = [];
+                    let curPoint    = [index_x, index_y],
+                        totalDist   = 0,
+                        distances   = [];
 
                     for (const point of input2) {
-                      distances.push([pointId(point), dist(point, curPoint)]);
+                        let distance = dist(point, curPoint);
+                        distances.push([pointId(point), distance]);
+                        totalDist += distance;
                     }
+
+                    if (totalDist < 10000) inRegion++;
                     
                     sortBy(distances, a => a[1]);
                     if (distances[0][1] === distances[1][1]) continue;
@@ -53,6 +60,7 @@ function day6() {
                 t1 = performance.now();
 
                 $("#first_result").text(Math.max(...Object.values(closest)));
+                $("#second_result").text(inRegion);
                 $("#timer").text(t1-t0 + ' miliseconds');
             }
         }
